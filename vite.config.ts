6b99bb7ea860a9eff16931008a964d39de9a4965
Justy Null <me@justyn.is>
2025-04-08 21:03:16 -0700

    fix: revert "feat: remove react-router-hono-server"
    This reverts commit 862433f3b9e5de744fca3925e7b4dc966d74f551.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import babel from "vite-plugin-babel";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
 
 export default defineConfig({
   plugins: [
@@ -16,6 +17,7 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler", { target: "19" }]],
       },
     }),
+    reactRouterHonoServer(),
     reactRouter(),
     tsconfigPaths(),
   ],
