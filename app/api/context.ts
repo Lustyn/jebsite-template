690e468370cfdb7f62ca54769d65c50935ee140d
Todd <hugeblank@gmail.com>
2025-05-02 18:26:17 -0700

    feat: include response headers in trpc context (#29)
    * feat: include response headers in trpc context
    
    * fix: apply headers change to pre-fetch
    
    * chore: adjust comment
    
    ---------
    
    Co-authored-by: Justy <justync9@gmail.com>

--- app/api/context.ts
+++ app/api/context.ts
@@ -1,9 +1,15 @@
-export function createContext({ req }: { req: Request }) {
+export function createContext({
+  req,
+  resHeaders,
+}: {
+  req: Request;
+  resHeaders: Headers;
+}) {
   // As an example, you can retrieve auth or other information here.
   // const user = { name: req.headers.get("username") ?? "anonymous" };
-
   return {
     req,
+    resHeaders,
     // user,
   };
 }
