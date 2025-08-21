1b00b18da2c4d6cfebf3212079f873461b40f34c
Justy Null <me@justyn.is>
2025-08-20 23:54:05 -0700

    fix: re-enable react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { __INTERNAL_DO_NOT_USE_OR_YOU_WILL_GET_A_STRONGLY_WORDED_LETTER__ } from
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-// import babel from "vite-plugin-babel";
+import babel from "vite-plugin-babel";
 
 const { unstable_reactRouterRSC } =
   __INTERNAL_DO_NOT_USE_OR_YOU_WILL_GET_A_STRONGLY_WORDED_LETTER__;
@@ -10,16 +10,18 @@ const { unstable_reactRouterRSC } =
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    // BUG: RSCs currently break React compiler
-    // babel({
-    //   filter: /\.[jt]sx?$/,
-    //   loader: "jsx",
-    //   babelConfig: {
-    //     compact: false,
-    //     presets: ["@babel/preset-typescript"],
-    //     plugins: [["babel-plugin-react-compiler", { target: "19" }]],
-    //   },
-    // }),
+    {
+      ...babel({
+        filter: /\.[jt]sx?$/,
+        loader: "jsx",
+        babelConfig: {
+          compact: false,
+          presets: ["@babel/preset-typescript"],
+          plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+        },
+      }),
+      applyToEnvironment: (environment) => environment.name === "client",
+    },
     unstable_reactRouterRSC(),
     tsconfigPaths(),
   ],
